afa7d56da893e55b3e31fe4f72297e8d8dc9a3e3
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-07-20 18:46:16 +0530

    Initial commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,7 +33,7 @@ export const login=async(req,res)=>{
     res.cookie('token',token,{
         httpOnly:true,
         sameSite:  "none",
-        secure: process.env.NODE_ENV === "production", 
+        secure: true, 
         expires:new Date(Date.now()+oneDay),
       
     })

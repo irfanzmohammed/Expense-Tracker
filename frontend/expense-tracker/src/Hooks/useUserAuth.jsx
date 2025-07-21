6cd83c32b5e2ad03b218590754716585c62006af
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-07-21 20:47:10 +0530

    Initial commit

--- frontend/expense-tracker/src/Hooks/useUserAuth.jsx
+++ frontend/expense-tracker/src/Hooks/useUserAuth.jsx
@@ -1,41 +1,44 @@
-import React, { useContext, useEffect,useState } from 'react'
-
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import axiosInstance from '../utils/axiosInstance';
 const useUserAuth = () => {
-     const { user, clearUser, updateUser } = useContext(UserContext);
-  const [loading, setLoading] = useState(true);
+    const {user,clearUser,updateUser}=useContext(UserContext)
+    const navigate=useNavigate();
 
-  useEffect(() => {
-    let isMounted = true;
+    useEffect(() => {
+        if (user) return;
+    
+        let isMounted = true;
+    
+        const getUser = async () => {
+          try {
+            const response = await axiosInstance.get(
+              import.meta.env.VITE_REACT_APP_GET_USER
+            );
+    
+            if (isMounted && response.data) {
+              updateUser(response.data.user); 
 
-    const getUser = async () => {
-      try {
-        const response = await axiosInstance.get(
-          import.meta.env.VITE_REACT_APP_GET_USER
-        );
-
-        if (isMounted && response.data?.user) {
-          updateUser(response.data.user);
-        }
-      } catch (error) {
-        if (isMounted) {
-          clearUser();
-        }
-      } finally {
-        if (isMounted) setLoading(false);
-      }
+            }
+          } catch (error) {
+            console.error('Error fetching user data', error);
+            if (isMounted) {
+              clearUser();
+              navigate('/auth');
+            }
+          }
+        };
+    
+        getUser();
+    
+        return () => {
+          isMounted = false;
+        };
+      }, [user,updateUser, clearUser, navigate]);
+      return user;
     };
 
-    getUser();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [user, updateUser, clearUser]);
-
-  return { user, loading };
-};
 
 
 export default useUserAuth
